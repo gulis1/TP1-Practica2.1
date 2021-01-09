0dc0cad4a9a89b3c115c047fd6b01e0266c47a40
gulis1 <julian24082000@hotmail.com>
2021-01-09 18:43:27 +0100

    Ultimo comentario?¿

--- src/control/Exceptions/GameException.java
+++ src/control/Exceptions/GameException.java
@@ -1,6 +1,7 @@
 package control.Exceptions;
 // Superclase de todas las excepciones que surgen en el juego
 public class GameException extends Exception{
+    // Se le añaden al mensaje las señales de ERROR al principio y en cada salto de línea.
     public GameException(String text) {
         super("[ERROR] "+text.replace("\n","\n[ERROR]"));
     }
