410d00f5aa9e0155939a97c19bd0ea579d08a2ba
Jealbacal <72936190+Jealbacal@users.noreply.github.com>
2021-06-03 22:13:54 +0200

    Add files via upload

--- src/logic/GameObjects/Dracula.java
+++ src/logic/GameObjects/Dracula.java
@@ -6,7 +6,8 @@ import logic.Game;
 public class Dracula extends Vampire {
 
     private static boolean draculaAlive = false;
-    private String pito;
+    private String pitA;
+
     public Dracula(int x, int y, int hp, Game game) {
         super(x, y, hp, game);
         draculaAlive = true;
